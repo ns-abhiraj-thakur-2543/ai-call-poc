83640bf59278e916ac967c54b3b33285c80c9939
Abhiraj Thakur <abhiraj.thakur@netsmartz.com>
2026-07-02 21:15:30 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/poc-demo/',
   plugins: [react()],
+  define: {
+    // This automatically rewrites the hardcoded URL to the relative Nginx path ONLY during production build
+    'https://devai.careerforcepro.com/api': '"/poc-demo/api"', 
+  },
   server: {
     proxy: {
       "/api": {
